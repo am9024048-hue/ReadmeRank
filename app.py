e4b4722e310ecdb378aa63b3e8413c19ea91ba00
am9024048-hue <am9024048@gmail.com>
2026-09-11 01:35:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ if st.button("🚀 فحص وتقييم README", type="primary"):
                 """
 
                 response = client.models.generate_content(
-                    model='gemini-2.5-flash',
+                    model=gemini-1.5-flash
                     contents=prompt,
                 )
 
